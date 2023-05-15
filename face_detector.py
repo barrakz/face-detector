48b459ff5c1599e42e76bcb5da9cb805e1f0cb6d
barrakz <b.rakuzy@gmail.com>
2023-05-15 21:36:02 +0200

    face coordinated

--- face_detector.py
+++ face_detector.py
@@ -3,8 +3,13 @@ import cv2
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 img = cv2.imread('fs.jpg')
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('Face Detector App', img)
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+print(face_coordinates)
+
+cv2.imshow('Face Detector App', grayscaled_img)
 cv2.waitKey()
 
 print("Code completed")
