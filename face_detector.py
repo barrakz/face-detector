9f4ff75e66188f95364a7f902c9a691e68cc09d9
barrakz <b.rakuzy@gmail.com>
2023-05-15 22:12:22 +0200

    loop detector

--- face_detector.py
+++ face_detector.py
@@ -2,14 +2,18 @@ import cv2
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('fs.jpg')
+img = cv2.imread('tf.png')
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-print(face_coordinates)
+# Draw rectagles around the faces
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-cv2.imshow('Face Detector App', grayscaled_img)
+# print(face_coordinates)
+
+cv2.imshow('Face Detector App', img)
 cv2.waitKey()
 
 print("Code completed")
