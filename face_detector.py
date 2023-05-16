ee7c7a5a288c1bd9cc6a2b3b8f1d4cb2e499e3a0
barrakz <b.rakuzy@gmail.com>
2023-05-16 21:25:39 +0200

    update

--- face_detector.py
+++ face_detector.py
@@ -1,7 +1,6 @@
 import cv2
 
-trained_face_data = cv2.CascadeClassifier(
-    'haarcascade_frontalface_default.xml')
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 img = cv2.imread('tf.png')
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -10,9 +9,10 @@ face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
 # Draw rectagles around the faces
 for (x, y, w, h) in face_coordinates:
+ 
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-print(face_coordinates)
+
 
 cv2.imshow('Face Detector App', img)
 cv2.waitKey()
